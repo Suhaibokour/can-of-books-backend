d282beab5130e7545c432875792bc5df6ca18b4f
suhaibokour <okoursuhaib@gmail.com>
2021-09-13 20:33:55 +0300

    lab11-backend

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# can-of-books-backend
+# Project Name
+
+**Author**: Suhaib okour
+**Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
+
+## Overview
+<!-- Provide a high level overview of what this application is and why you are building it, beyond the fact that it's an assignment for this class. (i.e. What's your problem domain?) --> This app is for adding an authentication where the users are required to login using another app such as google. 
+
+## Getting Started
+<!-- What are the steps that a user must take in order to build this app on their own machine and get it running? --> They should create a react repo and use the auth0 decomentation do use their laibrary insdie their react app.
+
+## Architecture
+<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+Using ReactApp, auth0, VScode, ubunto. 
+
+## Change Log
+<!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an example:
+
+01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource. -->
+
+## Credit and Collaborations
+<!-- Give credit (and a link) to other people or resources that helped you build this application. -->
+My partner Yousef Obiedat with the help of our class mate Rihan foudeh and TA Boshra.
+
+Name of feature: _Auth0_______________________________
+
+Estimate of time needed to complete: __30 mintues___
+
+Start time: _1:00____
+
+Finish time: __1:40___
+
+Actual time needed to complete: ___45 minutes__
---
+++ server.js
@@ -0,0 +1,92 @@
+'use stricts'
+
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const server = express();
+
+server.use(cors());
+
+const PORT = process.env.PORT;
+
+
+// mongoDB
+
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+let Book;
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/books');
+
+    const bookSchema = new mongoose.Schema({
+        title: String,
+        description: String,
+        status: String,
+        email: String
+    });
+
+     Book = mongoose.model('Book', bookSchema);
+
+    // seedData();
+}
+
+async function seedData(){
+    const book1 = new Book({
+        title: ' The Art of Asking',
+        description: 'Is it okay to ask for help as an artist? Or should you sit back and meekly let things go by? Palmer answers these questions by sharing great stories from her career to show that it is, in fact, just fine to ask.',
+        status: 'by Amanda Palmer',
+        email: 'okoursuhaib@gmail.com'
+    });
+
+    const book2 = new Book({
+        title: 'Down and Out in Paris and London',
+        description: 'Orwell is known for his scary dystopian masterpiece, 1984, and his political commentary, Animal Farm, but his nonfiction is where all the life lessons are. Down and Out is a great story about his life in poverty, how he learned to cope with a world crumbling around him, and how he worked to get himself out of it. A powerful and encouraging read.',
+        status: 'by George Orwell',
+        email: 'okoursuhaib@gmail.com'
+    });
+
+    const book3 = new Book({
+        title: ' Girls to the Front',
+        description: 'Whether you’re a punk or not, this is a great book about the origin of a movement called Riot Grrrl and the impact it had on music and feminism. It showcases some of the upsides and downsides to early ’90s feminism, and also illustrates just how influential these female punk bands were for the future of music.',
+        status: 'by Sara Marcus',
+        email: 'okoursuhaib@gmail.com'
+    });
+    await book1.save();
+    await book2.save();
+    await book3.save();
+}
+
+
+
+
+// routes 
+server.get('/getBooks', getBooksHandler);
+
+server.get('/', homeHandler);
+
+
+
+
+// functions
+
+function homeHandler(req, res) {
+    res.send('home page');
+}
+
+function getBooksHandler(req,res){
+    let email=req.query.email;
+    Book.find({email:email},(err,result)=>{
+res.send(result);
+    })
+     
+}
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
+})
+
